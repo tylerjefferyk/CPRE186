e7b4883a957028dabfa6da44905aad40db7f9a9d
Krueger <tylerk@iastate.edu>
2017-01-17 17:07:17 -0600

    fixed length

--- dot.c
+++ dot.c
@@ -1,7 +1,7 @@
 double dot(double a[], double b[], int length) {
 	int i=0;
 	double sum=0;   
-	for (i=0; i<= length; i++) { 
+	for (i=0; i < length; i++) { 
 		sum = sum + a[i]*b[i];
 		}
 	return sum;
